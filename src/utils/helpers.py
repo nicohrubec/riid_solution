c7e6da6dbf57f1da1bed7786b844c9764952bb86
nicohrubec <nicolas.hrubec@outlook.com>
2020-12-21 08:45:47 +0100

    Implement full data training.
    Still some bug. After second tailing data gets reduced to half although it should not.
    Also still not tested whether it fits into mem.
    
    Signed-off-by: nicohrubec <nicolas.hrubec@outlook.com>

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 from src.utils import configs
+from src.utils import hyperparameters as hp
 
 
 def replace_bools(df):
@@ -11,19 +12,42 @@ def replace_bools(df):
     return df
 
 
-def load_base_features(fold, mode, tail=False):
+def add_remaining_data(df, fold):
+    for i in range(hp.nfolds):
+        print("Add data for fold {}".format(i))
+        print(df.shape)
+        if i == fold:
+            continue
+        fold_path = configs.data_dir / 'fold{}.csv'.format(fold)
+        fold_df = pd.read_csv(fold_path)
+        fold_df = fold_df.groupby('user_id').tail(1000)
+
+        df = df.append(fold_df)
+
+    del fold_df
+    return df
+
+
+def load_base_features(fold, mode, tail=True, full=True):
     if mode == 'train':
         fold_path = configs.data_dir / 'fold{}_train.csv'.format(fold)
     elif mode == 'val':
         fold_path = configs.data_dir / 'fold{}_val.csv'.format(fold)
 
     df = pd.read_csv(fold_path)
+
+    if mode == 'train':
+        if full:
+            df = add_remaining_data(df, fold=fold)
+
     df = df[df.answered_correctly != -1]
+    print(df.shape)
 
     # only train on tails of user histories
     if tail:
         if mode == 'train':
             df = df.groupby('user_id').tail(1000)
+            print(df.shape)
 
     target = df['answered_correctly']
     df = replace_bools(df)
