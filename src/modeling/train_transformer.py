429f1934daabb6f517253d20f8a63c0d0524fe7d
nicohrubec <nicolas.hrubec@outlook.com>
2020-12-28 12:34:33 +0100

    Add working validation.
    Signed-off-by: nicohrubec <nicolas.hrubec@outlook.com>

--- src/modeling/train_transformer.py
+++ src/modeling/train_transformer.py
@@ -20,7 +20,6 @@ def train_epoch(model, loader, optimizer, device, criterion):
     train_loss = 0.0
 
     for i, (history, sample, positions, target, mask) in enumerate(loader):
-        if i > 0: break
         history, sample, positions, target, mask = \
             history.to(device), sample.to(device), positions.to(device), target.to(device), mask.to(device)
         optimizer.zero_grad()
@@ -31,14 +30,41 @@ def train_epoch(model, loader, optimizer, device, criterion):
         loss.backward()
         optimizer.step()
 
-    return model
+        train_loss += loss.item() / len(loader)
+
+    return model, train_loss
+
+
+def validate(model, loader, device, criterion):
+    model.eval()
+    val_loss = 0.0
+    val_preds = []
+    val_targets = []
+
+    with torch.no_grad():
+        for i, (history, sample, positions, target, mask) in enumerate(loader):
+            history, sample, positions, target, mask = \
+                history.to(device), sample.to(device), positions.to(device), target.to(device), mask.to(device)
+
+            preds = model(history, sample, positions, mask)
+            loss = criterion(preds, target)
+            val_loss += loss.item() / len(loader)
+
+            val_preds.append(preds.cpu())
+            val_targets.append(target.cpu())
+
+        val_preds = np.concatenate(val_preds)
+        val_targets = np.concatenate(val_targets)
+        val_auc = roc_auc_score(val_targets, val_preds)
+
+    return val_loss, val_auc
 
 
 def train_transformer_fold(fold):
     helpers.set_seed(42)
     xtrn, ytrn = helpers.load_base_features(fold, mode='train')
     xval, yval = helpers.load_base_features(fold, mode='val')
-    n_questions = xtrn.content_id.max()
+    n_questions = max(xtrn.content_id.max(), xval.content_id.max())
 
     # group by users
     trn_group = xtrn[['user_id', 'content_id', 'answered_correctly']].groupby('user_id').apply(
@@ -53,7 +79,7 @@ def train_transformer_fold(fold):
     train_set = TransformerDataset(trn_group)
     val_set = TransformerDataset(val_group)
 
-    train_loader = DataLoader(train_set, batch_size=hp.batch_size, shuffle=False)
+    train_loader = DataLoader(train_set, batch_size=hp.batch_size, shuffle=True)
     val_loader = DataLoader(val_set, batch_size=hp.val_batch_size, shuffle=False)
 
     criterion = nn.BCEWithLogitsLoss()
@@ -66,13 +92,17 @@ def train_transformer_fold(fold):
     log_dir = configs.log_dir / datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     writer = SummaryWriter(log_dir=log_dir)
 
-    for epoch in range(hp.nepochs):
+    for epoch in range(1, hp.nepochs + 1):
         print(f"Train epoch {epoch}")
         epoch_path = checkpoint_path / f'fold{fold}_epoch{epoch}'
         model, loss = train_epoch(model, train_loader, optimizer, device, criterion)
+        val_loss, val_auc = validate(model, val_loader, device, criterion)
 
         writer.add_scalar("Train loss", loss, epoch)
-        print(f"Train loss: {loss}")
+        writer.add_scalar("Val loss", val_loss, epoch)
+        writer.add_scalar("Val auc", val_auc, epoch)
+        print("Train loss: {:5.5f}".format(loss))
+        print("Val loss: {:5.5f}    Val auc: {:4.4f}".format(val_loss, val_auc))
 
     writer.flush()
     writer.close()
--- src/utils/hyperparameters.py
+++ src/utils/hyperparameters.py
@@ -5,13 +5,13 @@ holdout_fold = .15
 unseen_fold = .5
 
 # modeling
-nepochs = 200
-batch_size = 128
-val_batch_size = batch_size * 8
+nepochs = 10
+batch_size = 512
+val_batch_size = batch_size * 2
 lr = 0.0001
 num_heads = 2
 num_enc_layers = 2
 max_seq = 100
-embed_size = 64
+embed_size = 128
 dropout = 0.1
 dim_ff = 256
