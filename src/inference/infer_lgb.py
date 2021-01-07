ccab783c5e7e8f8e6eabd21a6d86d989d8cc3c7a
nicohrubec <nicolas.hrubec@outlook.com>
2021-01-07 15:55:40 +0100

    Update lgb inference.
    Signed-off-by: nicohrubec <nicolas.hrubec@outlook.com>

--- src/inference/infer_lgb.py
+++ src/inference/infer_lgb.py
@@ -10,13 +10,19 @@ from src.utils import helpers, configs
 def infer_lgb(fold):
     feats = ['content_id', 'task_container_id', 'prior_question_elapsed_time', 'prior_question_had_explanation', 'part',
              'content_id_target_mean', 'user_count', 'user_correct_mean', 'user_question_count',
-             'user_question_correct_mean', 'last_time_user', 'last_time_question_user', 'last_time_user_inter']
+             'user_question_correct_mean', 'last_time_user', 'last_time_question_user', 'last_time_user_inter',
+             'user_last_n_correct', 'answer1', 'answer2', 'answer3', 'answer4', 'user_last_n_time', 'user_last_n_time2',
+             'user_last_n_time3', 'user_part_count', 'user_part_correct_mean', 'user_part1_mean', 'user_part2_mean',
+             'user_part3_mean', 'user_part4_mean', 'user_part5_mean', 'user_part6_mean', 'user_part7_mean',
+             'task_container_eq1', 'task_container_eq2',
+             'user_hardness_count', 'user_hardness_mean', 'user_hardness_inter']
     target = 'answered_correctly'
     xtrn, ytrn = helpers.load_base_features(fold, mode='train')
     xval, yval = helpers.load_base_features(fold, mode='val')
 
     # get features
     xtrn, xval = feature_engineering.get_global_stats(xtrn, xval, target)
+    xtrn, xval = feature_engineering.get_answer_feats(xtrn, xval)
     xtrn, xval = feature_engineering.get_user_feats(xtrn, xval)
 
     oof = infer(xval, yval, feats, fold)
@@ -25,7 +31,7 @@ def infer_lgb(fold):
 
 
 def infer(xval, yval, feats, fold):
-    model_path = configs.model_dir / 'lgb_7791_full_fold{}.dat'.format(fold)
+    model_path = configs.model_dir / 'lgb_7892_fold{}.dat'.format(fold)
     model = pickle.load(open(model_path, "rb"))
 
     val_preds = model.predict(xval[feats].values.astype(np.float32))
